6da79a902267b81161f80f2f49148d88daa93893
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2024-01-05 16:01:46 +0000

    deploy to us-east-1

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -3,13 +3,13 @@ set -e
 aws cloudformation deploy \
   --template-file ci/codepipeline.yml \
   --stack-name codepipeline-cfn-service-catalog-portfolio-share \
-  --capabilities CAPABILITY_IAM \
+  --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
   --region eu-west-1 \
   --profile service-catalog
 
 aws cloudformation deploy \
   --template-file ci/codepipeline.yml \
   --stack-name codepipeline-cfn-service-catalog-portfolio-share \
-  --capabilities CAPABILITY_IAM \
+  --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
   --region us-east-1 \
   --profile service-catalog
